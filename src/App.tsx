fa4d4bbfe3f7c0d0998df3f2902998cd7d3488dd
julinha13 <ju.amorim13@gmail.com>
2021-11-18 01:14:54 -0300

    chore: Cria componente demo de uso do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import Card from "./components/DemoCard";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Testing CI with GH Actions</p>
+        <Card />
       </header>
     </div>
   );
