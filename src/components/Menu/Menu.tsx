7c153799ecd773adabd259a24c6dd960f0141a35
julinha13 <ju.amorim13@gmail.com>
2021-11-18 21:37:58 -0300

    Integra MenuItem ao Menu para exemplo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { MenuContainer, TitleContainer, Title, TitleButton } from "./styles";
+import { useState, useReducer } from "react";
+
+import { FaChevronLeft, FaChevronRight, FaAddressCard } from "react-icons/fa";
+
+import MenuItem from "./MenuItem";
+
+import {
+  MenuContainer,
+  TitleContainer,
+  Title,
+  TitleButton,
+  MenuContent,
+} from "./styles";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState([true, false]);
+  const forceUpdate = useReducer(() => ({}), {})[1] as () => void;
+
   const handleTitleButtonClick = () => {
     setOpen(!open);
+  };
+
+  const handleMenuClick = (index: number) => {
+    const selectedMenuAux = selectedMenu;
+    selectedMenuAux.forEach((open, i) => {
+      if (i !== index) {
+        selectedMenuAux[i] = false;
+      } else {
+        selectedMenuAux[i] = true;
+      }
+    });
+    setSelectedMenu(selectedMenuAux);
+    forceUpdate();
   };
 
   return (
@@ -16,6 +42,24 @@ const Menu = () => {
           {open ? <FaChevronLeft /> : <FaChevronRight />}
         </TitleButton>
       </TitleContainer>
+      <MenuContent>
+        <MenuItem
+          itemName="Menu 1"
+          icon={<FaAddressCard />}
+          open={open}
+          position={0}
+          handleClickItem={handleMenuClick}
+          isSelected={selectedMenu[0]}
+        />
+        <MenuItem
+          itemName="Menu 2"
+          icon={<FaAddressCard />}
+          open={open}
+          position={1}
+          handleClickItem={handleMenuClick}
+          isSelected={selectedMenu[1]}
+        />
+      </MenuContent>
     </MenuContainer>
   );
 };
