bfad80649af2e6e15fc3f8b2ab8608262b13025f
Arthur Melo <arthurhsmelo@gmail.com>
2021-11-21 15:03:01 -0300

    add created by

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,9 +1,11 @@
 import { Album } from "./album.model";
 import { BaseEntity } from "./common.model";
 import { Photo } from "./photo.model";
+import { User } from "./user.model";
 
 export interface Comment extends BaseEntity {
   content: string;
   photo: Omit<Photo, "comments">;
   album: Omit<Album, "comments">;
+  createdBy: Omit<User, "comments">;
 }
