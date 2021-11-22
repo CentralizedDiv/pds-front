53e0fff79435cec86aae28de0631c0300c3566f9
julinha13 <ju.amorim13@gmail.com>
2021-11-22 13:04:54 -0300

    feat: create AlbumCard and add to dashboard page.

--- src/components/AlbumCard/AlbumCard.component.tsx
+++ src/components/AlbumCard/AlbumCard.component.tsx
@@ -1,14 +1,29 @@
-import { Container, AlbumImage, AlbumInfoContent } from "./AlbumCard.styles";
+import { TextCP, TextType } from "components/shared/text/text.component";
+import {
+  Container,
+  AlbumImage,
+  AlbumInfoContent,
+  AlbumNameContent,
+} from "./AlbumCard.styles";
 
-const AlbumCard = () => {
+interface IProps {
+  albumCoverImage: string;
+  albumName: string;
+  creationDate: string;
+}
+
+const AlbumCard = ({ albumCoverImage, albumName, creationDate }: IProps) => {
   return (
     <Container>
-      <AlbumImage
-        src={
-          "https://ichef.bbci.co.uk/news/640/cpsprodpb/15744/production/_118667878_ka_05_friendsreunion.jpg"
-        }
-      />
-      <AlbumInfoContent></AlbumInfoContent>
+      <AlbumImage src={albumCoverImage} />
+      <AlbumInfoContent>
+        <div>
+          <TextCP>{creationDate}</TextCP>
+        </div>
+        <AlbumNameContent>
+          <TextCP type={TextType.TITLE_48}>{albumName}</TextCP>
+        </AlbumNameContent>
+      </AlbumInfoContent>
     </Container>
   );
 };
