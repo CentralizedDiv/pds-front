61462b14de7c66056149d26ebe54b7b5c029f036
Arthur Melo <arthurhsmelo@gmail.com>
2022-01-07 22:01:11 -0300

    Filter photos based on pills

--- src/components/shared/input/input.component.tsx
+++ src/components/shared/input/input.component.tsx
@@ -1,21 +1,24 @@
 import React, { InputHTMLAttributes } from "react";
 import { black } from "../colors";
 import { TextCP } from "../text/text.component";
-import { Input, InputContainer } from "./input.styles";
+import { Input, InputContainer, InputIconContainer } from "./input.styles";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   label: string;
+  large?: boolean;
+  icon?: JSX.Element;
 }
 
 export const InputCP = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ id, label, style, ...props }: InputProps, ref) => {
+  ({ id, label, large = false, icon, style, ...props }: InputProps, ref) => {
     return (
       <InputContainer style={style} htmlFor={id}>
         <TextCP color={black} overrideStyles={{ fontWeight: 400 }}>
           {label}
         </TextCP>
-        <Input id={id} {...props} ref={ref} />
+        <Input id={id} {...props} ref={ref} large={large} withIcon={!!icon} />
+        {icon && <InputIconContainer>{icon}</InputIconContainer>}
       </InputContainer>
     );
   }
